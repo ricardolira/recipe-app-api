2dd013e38bbdbe26429d4c89768f60a6f4ddf117
ricardolira <r.liraf@gmail.com>
2019-08-12 21:00:44 -0300

    Add recipe application and minor tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from . import models
+
+def sample_user(email='test@example.com', password='testpass'):
+    """Creates a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class Modeltests(TestCase):
     def test_rete_user_with_email_successful(self):
@@ -35,3 +41,11 @@ class Modeltests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user()
+            name="Vegan"
+        )
+        self.assertEqual(str(tag), tag.name)
