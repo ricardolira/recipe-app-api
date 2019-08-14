cc9b9a65de592deb3b310004471fcab736afaf8b
ricardolira <r.liraf@gmail.com>
2019-08-14 12:53:07 -0300

    updated recipe api for uploading photos

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+from unittest.mock import patch
+
 
 def sample_user(email='test@example.com', password='testpass'):
     """Creates a sample user"""
@@ -69,3 +71,14 @@ class Modeltests(TestCase):
             price=5.00
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
